4f29f68fe1fbc5773fbbb816229cb0f02232bb4d
Thomas <thomas.tognacci@gmail.com>
2017-11-23 18:56:39 -0330

    html mockup

--- src/App.js
+++ src/App.js
@@ -6,13 +6,40 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" value="title"/>
+        <br/>
+        <textarea type="textarea" value="description"></textarea>
+        <div>
+          Results
+          <br/>
+          <input type="text" value="title"/>
+          <br/>
+          <textarea type="textarea" value="description"></textarea>
+          <br/>
+          <button>Add</button>
+        </div>
+        <div>
+          Questions 1 of x
+          <br/>
+          <input type="text" value="Question Text"/>
+          <br/>
+          <textarea type="textarea" value="Question Description"></textarea>
+          <br/>
+          Answers
+          <br/>
+          <input type="text" value="Answer title"/>
+          <br/>
+          <select>
+          <option>Assign a result</option>
+          </select>
+          <br/>
+          <button>Add Answer</button>
+          <br/>
+          <br/>
+          <button>Add Question</button>
+        </div>
+
+
       </div>
     );
   }
