6abffa4afea3744e510bbd02847e325802f5d9e3
DESKTOP-KMFNE6L\Adam <adam.lichter@radient360.com>
2017-11-23 20:01:07 -0330

    added section component, answer component, and button

--- src/App.js
+++ src/App.js
@@ -5,49 +5,93 @@ import './App.css';
 class TitleAndDescription extends Component {
   constructor(props) {
     super(props);
-
   }
   render() {
     return (
       <div>
-        <input type="text" placeholder={this.props.title}/>
-        <br/>
+        <input type="text" placeholder={this.props.title} />
+        <br />
         <textarea type="textarea" placeholder={this.props.description}></textarea>
       </div>
     )
   }
 }
 
+class Answers extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // for result in results {
+    //  do results
+    //}
+
+    //}
+    return (
+      <div>
+        <input type="text" placeholder={this.props.title} />
+        <br />
+        <select>
+          <option>item</option>
+        </select>
+      </div>
+    )
+  }
+}
+
+function Button(props) {
+  return (
+    <button onClick={props.onClick}>{props.name}</button>
+  );
+}
+
+function duplicate(element) {
+
+}
+
+function Section(props) {
+  return (
+    <div>
+      {props.children}
+      <Button onClick='' name={props.buttonName}/>
+    </div>
+  )
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TitleAndDescription title="Quizz Title" description="Quizz Description"/>
-        <div>
-          Results
-          <br/>
-          <TitleAndDescription title="Result Title" description="Result Description"/>
-          <br/>
-          <button>Add</button>
-        </div>
-        <div>
-          Questions 1 of x
-          <br/>
-          <TitleAndDescription title="Question Title" description="Question Description"/>
-          <br/>
+
+        <TitleAndDescription title='Quizz Title' description="Quizz Description" />
+
+        Results
+        <Section buttonName='add +'>
+          <TitleAndDescription title="Result Title" description="Result Description" />
+        </Section>
+
+        Questions
+        <Section buttonName='add +'>
+          <TitleAndDescription title="Question Title" description="Question Description" />
           Answers
-          <br/>
-          <input type="text" value="Answer title"/>
-          <br/>
+          <Section buttonName='add +'>
+            <Answers title='Answers'/>
+          </Section>
+        </Section>
+
+        
+          {/* Answers
+          <br />
+          <input type="text" value="Answer title" />
+          <br />
           <select>
-          <option>Assign a result</option>
+            <option>Assign a result</option>
           </select>
-          <br/>
+          <br />
           <button>Add Answer</button>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <button>Add Question</button>
-        </div>
+        </div> */}
 
 
       </div>
@@ -56,3 +100,4 @@ class App extends Component {
 }
 
 export default App;
+
