3adb1de6d4b47a3cc36397173a763f91c91700b3
DESKTOP-KMFNE6L\Adam <adam.lichter@radient360.com>
2017-11-30 21:07:23 -0330

    adding quiz taking module

---
+++ src/components/displayQuestions.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+// Custom Components
+import { Button } from './Button';
+import { TitleAndDescription } from './TitleAndDescription';
+import { Section } from './Section'
+
+export class DisplayQuestions extends Component {
+    
+    render() {
+        let answerButtons = this.props.questions.answers.map((x) => <Button name={x.title} />)
+        return (
+            <div className="question-answer">
+                <Section>
+                    <TitleAndDescription
+                        title= {this.props.questions.title}
+                        description={this.props.questions.description}
+                        notEditable
+                    />
+                </Section>
+                {answerButtons}
+            </div>
+
+        )
+    }
+
+}
